3bab802913572747cf3a66cc1aea2fdd9fa71fc6
vigne <vigne@Vignesh>
2025-09-23 19:45:01 +0530

    Completed

---
+++ core_java_program/src/Day1/IdentifersDemo.java
@@ -0,0 +1,12 @@
+package Day1;
+
+public class IdentifersDemo {
+
+	public static void main(String[] args) {
+		int $number = 20;
+		System.out.println("value of the number variable is : "+ $number);
+		String studentName="Aniket"; 
+		System.out.println("value is : "+ studentName);
+	}
+
+}
